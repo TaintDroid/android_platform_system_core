65e5b6ac26105c9dbe145cb87476adc1a5da1900
Peter Gilbert <petergilbert@gmail.com>
2012-12-06 11:42:36 -0500

    xattr support for FUSE emulated sdcard

--- sdcard/Android.mk
+++ sdcard/Android.mk
@@ -2,6 +2,11 @@ LOCAL_PATH:= $(call my-dir)
 
 include $(CLEAR_VARS)
 
+# Turn on Taint Tracking
+ifeq ($(WITH_TAINT_TRACKING),true)
+  LOCAL_CFLAGS += -DWITH_TAINT_TRACKING
+endif
+
 LOCAL_SRC_FILES:= sdcard.c
 LOCAL_MODULE:= sdcard
 
